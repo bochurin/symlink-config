4d7f1894ba057451f1fdc21b50de27fa707bcd27
bochurin <bochurin@gmail.com>
2025-10-12 17:15:05 +0400

    shore: unused scripts removed

--- not-used/types/index.ts
+++
@@ -1,9 +0,0 @@
-export type SymlinkMode = 'full' | 'clean' | 'dry'
-
-export type SymlinkType = 'file' | 'dir'
-
-export interface ProcessResult {
-  success: boolean
-  message: string
-  details?: string[]
-}
