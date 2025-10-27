1bc34a245b02f8e22ed445b638ff51e9e0858d0d
Yaroslav Bochurin <bochurin@gmail.com>
2025-10-27 16:50:27 +0400

    Update project code

--- selected/create-symlinks.bat
+++ selected/create-symlinks.bat
@@ -6,18 +6,17 @@ cd /d "%~dp0.." || exit /b 1
 
 REM Root files
 mkdir "selected\root"
-mklink "selected\root\package.json" "..\package.json"
-mklink "selected\root\README.md" "..\README.md"
-mklink "selected\root\symlink-config.code-workspace" "..\symlink-config.code-workspace"
-mklink "selected\root\_gitignore" "..\.gitignore"
-mklink "selected\root\tsconfig.json" "..\tsconfig.json"
+mklink "selected\root\package.json" "..\..\package.json"
+mklink "selected\root\symlink-config.code-workspace" "..\..\symlink-config.code-workspace"
+mklink "selected\root\_gitignore" "..\..\.gitignore"
+mklink "selected\root\tsconfig.json" "..\..\tsconfig.json"
 
 REM Source files
 mkdir "selected\src"
-mklink "selected\src\main.ts" "..\src\main.ts"
+mklink "selected\src\main.ts" "..\..\src\main.ts"
 
 REM Test workspace files
 mkdir "selected\test-ws-win"
-mklink "selected\test-ws-win\vscode_-_settings.json" "..\test-ws-win\.vscode\settings.json"
+mklink "selected\test-ws-win\vscode_-_settings.json" "..\..\test-ws-win\.vscode\settings.json"
 
 pause
--- selected/create-symlinks.sh
+++ selected/create-symlinks.sh
@@ -7,21 +7,20 @@ cd /home/bochu/projects/symlink-config/selected || exit 1
 echo "Creating root directory..."
 mkdir -p "root"
 echo "Linking root files..."
-ln -sfv "../package.json" "root/package.json"
-ln -sfv "../README.md" "root/README.md"
-ln -sfv "../symlink-config.code-workspace" "root/symlink-config.code-workspace"
-ln -sfv "../.gitignore" "root/_gitignore"
-ln -sfv "../tsconfig.json" "root/tsconfig.json"
+ln -sfv "../../package.json" "root/package.json"
+ln -sfv "../../symlink-config.code-workspace" "root/symlink-config.code-workspace"
+ln -sfv "../../.gitignore" "root/_gitignore"
+ln -sfv "../../tsconfig.json" "root/tsconfig.json"
 
 # Source files
 echo "Creating src directory..."
 mkdir -p "src"
 echo "Linking source files..."
-ln -sfv "../src/main.ts" "src/main.ts"
+ln -sfv "../../src/main.ts" "src/main.ts"
 
 # Test workspace files
 echo "Creating test-ws-unix directory..."
 mkdir -p "test-ws-unix"
 echo "Linking test workspace files..."
-ln -sfv "../test-ws-unix/.vscode/settings.json" "test-ws-unix/vscode_-_settings.json"
+ln -sfv "../../test-ws-unix/.vscode/settings.json" "test-ws-unix/vscode_-_settings.json"
 echo "Done!"
