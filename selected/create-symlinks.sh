8c88d23db7177d2342d10bc0e7ad27f4c66ed153
Yaroslav Bochurin <bochurin@gmail.com>
2025-10-27 15:40:07 +0400

    Update project code

---
+++ selected/create-symlinks.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Applying symlink configuration..."
+
+cd /home/bochu/projects/symlink-config/selected || exit 1
+
+# Root files
+echo "Creating root directory..."
+mkdir -p "root"
+echo "Linking root files..."
+ln -sfv "../package.json" "root/package.json"
+ln -sfv "../README.md" "root/README.md"
+ln -sfv "../symlink-config.code-workspace" "root/symlink-config.code-workspace"
+ln -sfv "../.gitignore" "root/_gitignore"
+ln -sfv "../tsconfig.json" "root/tsconfig.json"
+
+# Source files
+echo "Creating src directory..."
+mkdir -p "src"
+echo "Linking source files..."
+ln -sfv "../src/main.ts" "src/main.ts"
+
+# Test workspace files
+echo "Creating test-ws-unix directory..."
+mkdir -p "test-ws-unix"
+echo "Linking test workspace files..."
+ln -sfv "../test-ws-unix/.vscode/settings.json" "test-ws-unix/vscode_-_settings.json"
+echo "Done!"
